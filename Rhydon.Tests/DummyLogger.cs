5a2ac2d95d48c047c01b595ef4e23f20082e5d0b
SilentHammerHUN <zsirayricsi@gmail.com>
2019-08-01 11:36:40 +0200

    Add tests

---
+++ Rhydon.Tests/DummyLogger.cs
@@ -0,0 +1,25 @@
+﻿using Rhydon.Core;
+
+namespace Rhydon.Tests {
+    public class DummyLogger : ILogger {
+        public void Debug(string message) {
+            //
+        }
+
+        public void Info(string message) {
+            //
+        }
+
+        public void Warning(string message) {
+            //
+        }
+
+        public void Error(string message) {
+            //
+        }
+
+        public void Success(string message) {
+            //
+        }
+    }
+}
--- Rhydon.Tests/Parser.cs
+++ Rhydon.Tests/Parser.cs
@@ -1,5 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using dnlib.DotNet;
+using dnlib.DotNet.Emit;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Rhydon.Core;
 using Rhydon.Core.Parser;
@@ -13,11 +15,65 @@ namespace Rhydon.Tests {
             var ms = new MemoryStream();
             mod.Write(ms);
 
-            var ctx = new RhydonContext { Module = ModuleDefMD.Load(ms) };
+            var ctx = new RhydonContext { Module = ModuleDefMD.Load(ms), Logger = new DummyLogger() };
             var header = new KoiHeader(ctx);
             Assert.IsFalse(header.Good);
 
             ms.Close();
+        }
+
+        [TestMethod]
+        public void TestValidOpCodeMap() {
+            var random = new Random();
+
+            var mod = new ModuleDefUser("test");
+            var type = new TypeDefUser("Constants");
+            for (var i = 0; i < 119; i++)
+                type.Fields.Add(new FieldDefUser("randomName" + random.Next(), new FieldSig(mod.CorLibTypes.Byte)));
+
+            mod.Types.Add(type);
+
+            var ctor = type.FindOrCreateStaticConstructor();
+            var body = new CilBody();
+            body.Instructions.Insert(0, Instruction.Create(OpCodes.Stfld, type.Fields[0]));
+            body.Instructions.Insert(0, Instruction.Create(OpCodes.Ldc_I4, 112));
+            body.Instructions.Insert(0, Instruction.Create(OpCodes.Ldnull));
+            for (var i = 0; i < 118; i++) {
+                body.Instructions.Insert(0, Instruction.Create(OpCodes.Stfld, type.Fields[i]));
+                body.Instructions.Insert(0, Instruction.Create(OpCodes.Ldc_I4, random.Next(0, 0xFF)));
+                body.Instructions.Insert(0, Instruction.Create(OpCodes.Ldnull));
+            }
+            body.Instructions.Add(Instruction.Create(OpCodes.Ret));
+            ctor.Body = body;
+
+            var ms = new MemoryStream();
+            mod.Write(ms);
+
+            var ctx = new RhydonContext { Module = ModuleDefMD.Load(ms), Logger = new DummyLogger() };
+            var map = OpCodeMap.Parse(ctx);
+            Assert.IsNotNull(map);
+            Assert.IsTrue(map[112] == KoiOpCodes.REG_R0);
+
+            ms.Close();
+        }
+
+        [TestMethod]
+        public void TestInvalidOpCodeMap() {
+            var random = new Random();
+
+            var mod = new ModuleDefUser("test");
+            var type = new TypeDefUser("Constants");
+            for (var i = 0; i < 159; i++)
+                type.Fields.Add(new FieldDefUser("randomName" + random.Next(), new FieldSig(mod.CorLibTypes.Byte)));
+
+            mod.Types.Add(type);
+
+            var ms = new MemoryStream();
+            mod.Write(ms);
+
+            var ctx = new RhydonContext { Module = ModuleDefMD.Load(ms), Logger = new DummyLogger() };
+            var map = OpCodeMap.Parse(ctx);
+            Assert.IsNull(map);
         }
     }
 }
