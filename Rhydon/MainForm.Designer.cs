6ba8e1d39a8c815951f0047136fb76c64b38413c
SilentHammerHUN <zsirayricsi@gmail.com>
2019-08-02 19:50:50 +0200

    Basic WinForms UI

---
+++ Rhydon/MainForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Rhydon {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.OpacityTimer = new System.Windows.Forms.Timer(this.components);
+            this.Titlebar = new System.Windows.Forms.Panel();
+            this.LblTitle = new System.Windows.Forms.Label();
+            this.BtnClose = new System.Windows.Forms.Button();
+            this.BtnMinimize = new System.Windows.Forms.Button();
+            this.Titlebar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // OpacityTimer
+            // 
+            this.OpacityTimer.Enabled = true;
+            this.OpacityTimer.Interval = 1;
+            this.OpacityTimer.Tick += new System.EventHandler(this.OpacityTimer_Tick);
+            // 
+            // Titlebar
+            // 
+            this.Titlebar.BackColor = System.Drawing.Color.Black;
+            this.Titlebar.Controls.Add(this.BtnMinimize);
+            this.Titlebar.Controls.Add(this.BtnClose);
+            this.Titlebar.Controls.Add(this.LblTitle);
+            this.Titlebar.ForeColor = System.Drawing.Color.White;
+            this.Titlebar.Location = new System.Drawing.Point(0, 0);
+            this.Titlebar.Name = "Titlebar";
+            this.Titlebar.Size = new System.Drawing.Size(802, 26);
+            this.Titlebar.TabIndex = 0;
+            this.Titlebar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TitleBarDrag);
+            // 
+            // LblTitle
+            // 
+            this.LblTitle.AutoSize = true;
+            this.LblTitle.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.LblTitle.Location = new System.Drawing.Point(3, 2);
+            this.LblTitle.Name = "LblTitle";
+            this.LblTitle.Size = new System.Drawing.Size(69, 21);
+            this.LblTitle.TabIndex = 0;
+            this.LblTitle.Text = "Rhydon";
+            this.LblTitle.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TitleBarDrag);
+            // 
+            // BtnClose
+            // 
+            this.BtnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnClose.ForeColor = System.Drawing.Color.Red;
+            this.BtnClose.Location = new System.Drawing.Point(683, 2);
+            this.BtnClose.Name = "BtnClose";
+            this.BtnClose.Size = new System.Drawing.Size(23, 23);
+            this.BtnClose.TabIndex = 1;
+            this.BtnClose.Text = "X";
+            this.BtnClose.UseVisualStyleBackColor = true;
+            this.BtnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // BtnMinimize
+            // 
+            this.BtnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnMinimize.ForeColor = System.Drawing.Color.White;
+            this.BtnMinimize.Location = new System.Drawing.Point(658, 2);
+            this.BtnMinimize.Name = "BtnMinimize";
+            this.BtnMinimize.Size = new System.Drawing.Size(23, 23);
+            this.BtnMinimize.TabIndex = 2;
+            this.BtnMinimize.Text = "-";
+            this.BtnMinimize.UseVisualStyleBackColor = true;
+            this.BtnMinimize.Click += new System.EventHandler(this.BtnMinimize_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 308);
+            this.Controls.Add(this.Titlebar);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Rhydon";
+            this.ResizeBegin += new System.EventHandler(this.MainWindow_ResizeBegin);
+            this.ResizeEnd += new System.EventHandler(this.MainWindow_ResizeEnd);
+            this.Titlebar.ResumeLayout(false);
+            this.Titlebar.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer OpacityTimer;
+        private System.Windows.Forms.Panel Titlebar;
+        private System.Windows.Forms.Label LblTitle;
+        private System.Windows.Forms.Button BtnClose;
+        private System.Windows.Forms.Button BtnMinimize;
+    }
+}
+
---
+++ Rhydon/MainForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Rhydon {
+    public partial class MainForm : Form {
+        public MainForm() {
+            InitializeComponent();
+        }
+
+        #region Custom titlebar
+        [DllImport("user32.dll")]
+        static extern int SendMessage(IntPtr hWnd,
+            int msg, int wParam, int lParam);
+        [DllImport("user32.dll")]
+        static extern bool ReleaseCapture();
+
+        private void TitleBarDrag(object sender, MouseEventArgs e) {
+            if (e.Button != MouseButtons.Left)
+                return;
+
+            ReleaseCapture();
+            SendMessage(Handle, 0xA1, 0x2, 0);
+        }
+        #endregion
+
+        #region Opacity while moving window
+        private static bool _isMoving;
+        private void MainWindow_ResizeBegin(object sender, EventArgs e) =>
+            _isMoving = true;
+
+        private void MainWindow_ResizeEnd(object sender, EventArgs e) =>
+            _isMoving = false;
+
+        private void OpacityTimer_Tick(object sender, EventArgs e) {
+            if (_isMoving) {
+                if (Opacity > 0.40)
+                    Opacity -= 0.03;
+            } else {
+                if (Opacity < 1)
+                    Opacity += 0.04;
+            }
+        }
+        #endregion
+
+        #region Background color
+        private readonly Color _start = Color.FromArgb(255, 64, 64, 64);
+        private readonly Color _end = Color.FromArgb(255, 160, 32, 32);
+        protected override void OnPaint(PaintEventArgs e) {
+            base.OnPaint(e);
+            using (var brush = new LinearGradientBrush(ClientRectangle, _start, _end, 90F)) {
+                e.Graphics.FillRectangle(brush, ClientRectangle);
+            }
+        }
+        #endregion
+
+        #region Custom close/minimize buttons
+        private void BtnClose_Click(object sender, EventArgs e) =>
+            Close();
+
+        private void BtnMinimize_Click(object sender, EventArgs e) =>
+            WindowState = FormWindowState.Minimized;
+    #endregion
+    }
+}
