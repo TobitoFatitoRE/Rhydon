010599ba885181b2d25fc8d52d4648343238b98b
SilentHammerHUN <zsirayricsi@gmail.com>
2019-08-03 17:00:34 +0200

    Simplify EmuContext

--- Rhydon.Emulator/EmuContext.cs
+++ Rhydon.Emulator/EmuContext.cs
@@ -1,34 +1,38 @@
 ﻿using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using Rhydon.Core;
 using Rhydon.Core.Parser;
 
 namespace Rhydon.Emulator {
-    class EmuContext {
+    class EmuContext : RhydonContext {
         internal EmuContext(RhydonContext ctx, MethodExport exp) {
-            Context = ctx;
             Export = exp;
-            Reader = ctx.Reader;
             MethodBody = new List<KoiInstruction>();
             Stack = new Stack<VMSlot>();
             Registers = new VMSlot[16];
             Handlers = new Dictionary<KoiOpCodes, IKoiHandler>();
+
+            Module = ctx.Module;
+            Reader = ctx.Reader;
+            Header = ctx.Header;
+            StartOffset = ctx.StartOffset;
+            Map = ctx.Map;
+            Decompiled = ctx.Decompiled;
+            Logger = ctx.Logger;
+            Parameters = ctx.Parameters;
         }
 
-        internal RhydonContext Context;
-        internal MethodExport Export;
-        internal BinaryReader Reader;
-        internal List<KoiInstruction> MethodBody;
-        internal Stack<VMSlot> Stack;
-        internal VMSlot[] Registers;
-        internal Dictionary<KoiOpCodes, IKoiHandler> Handlers;
+        internal MethodExport Export { get; }
+        internal List<KoiInstruction> MethodBody { get; }
+        internal Stack<VMSlot> Stack { get; }
+        internal VMSlot[] Registers { get; set; }
+        internal Dictionary<KoiOpCodes, IKoiHandler> Handlers { get; }
 
         internal IKoiHandler Lookup(byte code) =>
-            Handlers[Context.Map[code]];
+            Handlers[Map[code]];
 
         internal byte Lookup(KoiOpCodes code) =>
-            Context.Map.First(p => p.Value == code).Key;
+            Map.First(p => p.Value == code).Key;
 
         internal byte ReadByte() =>
             Reader.ReadKoiByte(Export);
